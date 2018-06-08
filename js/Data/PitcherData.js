2e317d2987a5a24cdb0fdc5bff7a7f898fd16554
Evan Cripe <encripe@gmail.com>
2018-06-07 23:40:25 -0400

    PitcherData Edited

--- js/Data/PitcherData.js
+++ js/Data/PitcherData.js
@@ -1,20 +1,192 @@
 var pitcherData = [
     {
-        "id": "1001",
-        "firstName": "Kendra",
-        "lastName": "Stevens",
-        "teamId":"UK",
-        "batHand":"R",
-        "throwHand":"R",
-        "freshmanYear":"14"
+        "pitcherId": "LOUAdamWolf",
+        "firstName": "Adam",
+        "lastName": "Wolf",
+        "teamId":"LOU",
+        "batHand":"L",
+        "throwHand":"L",
+        "freshmanYear":"2016"
+        "position":"P"
     },
     {
-        "id": "1000",
-        "firstName": "Deanna",
-        "lastName": "Kaparo",
+        "pitcherId": "LOUNickBennett",
+        "firstName": "Nick",
+        "lastName": "Bennett",
+        "teamId":"LOU",
+        "batHand":"L",
+        "throwHand":"L",
+        "freshmanYear":"2017"
+        "position":"P"
+    },
+    {
+        "pitcherId": "LOUBobbyMiller",
+        "firstName": "Bobby",
+        "lastName": "Miller",
         "teamId":"LOU",
         "batHand":"R",
         "throwHand":"R",
-        "freshmanYear":"11"
+        "freshmanYear":"2018"
+        "position":"P"
+    },
+    {
+        "pitcherId": "LOUReidDetmers",
+        "firstName": "Reid",
+        "lastName": "Detmers",
+        "teamId":"LOU",
+        "batHand":"L",
+        "throwHand":"L",
+        "freshmanYear":"2018"
+        "position":"P"
+    },
+    {
+        "pitcherId": "LOUMichaelKirian",
+        "firstName": "Michael",
+        "lastName": "Kirian",
+        "teamId":"LOU",
+        "batHand":"L",
+        "throwHand":"L",
+        "freshmanYear":"2018"
+        "position":"P"
+    },
+    {
+        "pitcherId": "LOU",
+        "firstName": "",
+        "lastName": "",
+        "teamId":"LOU",
+        "batHand":"",
+        "throwHand":"",
+        "freshmanYear":"201"
+        "position":"P"
+    },
+    {
+        "pitcherId": "LOURileyThompson",
+        "firstName": "Riley",
+        "lastName": "Thompson",
+        "teamId":"LOU",
+        "batHand":"R",
+        "throwHand":"R",
+        "freshmanYear":"2016"
+        "position":"P"
+    },
+    {
+        "pitcherId": "LOUBryanHoeing",
+        "firstName": "Bryan",
+        "lastName": "Hoeing",
+        "teamId":"LOU",
+        "batHand":"R",
+        "throwHand":"R",
+        "freshmanYear":"2016"
+        "position":"P"
+    },
+    {
+        "pitcherId": "LOULiamJenkins",
+        "firstName": "Liam",
+        "lastName": "Jenkins",
+        "teamId":"LOU",
+        "batHand":"R",
+        "throwHand":"R",
+        "freshmanYear":"2016"
+        "position":"P"
+    },
+    {
+        "pitcherId": "LOUJDMundt",
+        "firstName": "JD",
+        "lastName": "Mundt",
+        "teamId":"LOU",
+        "batHand":"L",
+        "throwHand":"L",
+        "freshmanYear":"2017"
+        "position":"P"
+    },
+    {
+        "pitcherId": "LOUMacWelsh",
+        "firstName": "Mac",
+        "lastName": "Welsh",
+        "teamId":"LOU",
+        "batHand":"",
+        "throwHand":"",
+        "freshmanYear":"2015"
+        "position":"P"
+    },
+    {
+        "pitcherId": "LOURabonMartin",
+        "firstName": "Rabon",
+        "lastName": "Martin",
+        "teamId":"LOU",
+        "batHand":"L",
+        "throwHand":"L",
+        "freshmanYear":"2016"
+        "position":"P"
+    },
+    {
+        "pitcherId": "LOUShaySmiddy",
+        "firstName": "Shay",
+        "lastName": "Smiddy",
+        "teamId":"LOU",
+        "batHand":"R",
+        "throwHand":"R",
+        "freshmanYear":"2017"
+        "position":"P"
+    }
+    {
+        "pitcherId": "LOUAdamElliott",
+        "firstName": "Adam",
+        "lastName": "Elliott",
+        "teamId":"LOU",
+        "batHand":"L",
+        "throwHand":"L",
+        "freshmanYear":"2017"
+        "position":"P/OF"
+    }
+    {
+        "pitcherId": "LOUAustinConway",
+        "firstName": "Austin",
+        "lastName": "Conway",
+        "teamId":"LOU",
+        "batHand":"R",
+        "throwHand":"R",
+        "freshmanYear":"2014"
+        "position":"P"
+    }
+    {
+        "pitcherId": "LOUSamBordner",
+        "firstName": "Sam",
+        "lastName": "Bordner",
+        "teamId":"LOU",
+        "batHand":"R",
+        "throwHand":"R",
+        "freshmanYear":"2016"
+        "position":"P"
+    }
+    {
+        "pitcherId": "LOUGavinSullivan",
+        "firstName": "Gavin",
+        "lastName": "Sullivan",
+        "teamId":"LOU",
+        "batHand":"L",
+        "throwHand":"L",
+        "freshmanYear":"2018"
+        "position":"P"
+    }
+    {
+        "pitcherId": "LOUAustinDickey",
+        "firstName": "Austin",
+        "lastName": "Dickey",
+        "teamId":"LOU",
+        "batHand":"R",
+        "throwHand":"R",
+        "freshmanYear":"2017"
+        "position":"P"
+    }
+    {
+        "pitcherId": "LOUStewartCurrie",
+        "firstName": "Stewart",
+        "lastName": "Currie",
+        "teamId":"LOU",
+        "batHand":"R",
+        "throwHand":"R",
+        "freshmanYear":"2017"
+        "position":"P"
     }
 ];
